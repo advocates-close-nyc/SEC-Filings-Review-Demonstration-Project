1367a5bdd3d1fae09dba244b1eafb84ddcf7cffe
javaad.ali.ny@gmail.com <javaad101>
2024-12-11 08:41:52 -0600

    Initial Commit

---
+++ SEC-Filings-Review-Demonstration-Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SEC-Filings-Review-Demonstration-Project
+//
+//  Created by AdvocatesClose on 12/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SEC-Filings-Review-Demonstration-Project/SEC_Filings_Review_Demonstration_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SEC_Filings_Review_Demonstration_ProjectApp.swift
+//  SEC-Filings-Review-Demonstration-Project
+//
+//  Created by AdvocatesClose on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SEC_Filings_Review_Demonstration_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
